0daa7366dd5ef2e5188238e63d52e2878ee49c66
ceigyx <aklypse_ceigyx@live.ca>
2020-07-01 04:23:52 -0400

    completed logger function, reusable through all math operations

--- basics/assets/scripts/app.js
+++ basics/assets/scripts/app.js
@@ -1,54 +1,60 @@
 // Globals
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 // Logger
-let logEntries= [];
+function writeToLog(
+  operationIdentifier, 
+  prevResult, 
+  operationNumber, 
+  newResult) {
+    logEntries.push({
+      operation: operationIdentifier,
+      prevResult: prevResult,
+      number: operationNumber,
+      result: newResult,
+    });
+    console.log(logEntries);
+  }
 
-
-// Render 
-
+// Renderer
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
   outputResult(currentResult, calcDescription); //vendor.js
 }
 
 //Math operations
-
 function add() {
   const enteredNumber = getUserInput();
   const initialResult = currentResult;
   currentResult += +userInput.value;
-  createAndWriteOutput("+", initialResult, enteredNumber)
-  const logEntry = {
-    "operation": "ADD",
-    "prevResult": initialResult,
-    "number": enteredNumber,
-    "result":  currentResult
-  };
-  logEntries.push(logEntry);
-  console.log(logEntries);
+  createAndWriteOutput("+", initialResult, enteredNumber);
+  writeToLog("add", initialResult, enteredNumber, currentResult);
 }
 
 function subtract() {
   const enteredNumber = getUserInput();
   const initialResult = currentResult;
   currentResult -= +userInput.value;
-  createAndWriteOutput("-", initialResult, enteredNumber)
+  createAndWriteOutput("-", initialResult, enteredNumber);
+  writeToLog("subtract", initialResult, enteredNumber, currentResult);
 }
 
 function multiply() {
   const enteredNumber = getUserInput();
   const initialResult = currentResult;
   currentResult *= +userInput.value;
-  createAndWriteOutput("*", initialResult, enteredNumber)
+  createAndWriteOutput("*", initialResult, enteredNumber);
+  writeToLog("multiply", initialResult, enteredNumber, currentResult);
 }
 
 function divide() {
   const enteredNumber = getUserInput();
   const initialResult = currentResult;
   currentResult /= +userInput.value;
-  createAndWriteOutput("/", initialResult, enteredNumber)
+  createAndWriteOutput("/", initialResult, enteredNumber);
+  writeToLog("divide", initialResult, enteredNumber, currentResult);
 }
 
 //getters
@@ -57,7 +63,7 @@ function getUserInput() {
 }
 
 //attach event listners to buttons
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', subtract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
