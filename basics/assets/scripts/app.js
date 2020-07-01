9cbc619e6f1b88614f3bdcc799ce6a4484e95aaa
ceigyx <aklypse_ceigyx@live.ca>
2020-07-01 03:40:48 -0400

    folder restructured, sections will only be held with one folder, deviated from course structure due to useless redundancy

---
+++ basics/assets/scripts/app.js
@@ -0,0 +1,50 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+// Render 
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
+//Math operations
+
+function add() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + parseInt(userInput.value);
+  createAndWriteOutput("+", initialResult, enteredNumber)
+}
+
+function subtract() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - parseInt(userInput.value);
+  createAndWriteOutput("-", initialResult, enteredNumber)
+}
+
+function multiply() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * parseInt(userInput.value);
+  createAndWriteOutput("*", initialResult, enteredNumber)
+}
+
+function divide() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / parseInt(userInput.value);
+  createAndWriteOutput("/", initialResult, enteredNumber)
+}
+
+//getters
+function getUserInput() {
+  return parseInt(userInput.value);
+}
+
+//attach event listners to buttons
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
