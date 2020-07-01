c59ce70c10de9a24d03d38439725797cafc9730e
ceigyx <aklypse_ceigyx@live.ca>
2020-07-01 00:57:51 -0400

    finished assignment basic 2

--- basics-assignment-2/assignment.js
+++ basics-assignment-2/assignment.js
@@ -1,1 +1,20 @@
 const task3Element = document.getElementById('task-3');
+task3Element.addEventListener("click", alertText);
+
+alertText();
+alertName("name");
+
+function alertText() {
+    let text = "text";
+    alert(text);  
+}
+        
+function alertName(name) {
+    alert(name);
+}
+
+function combinator(one, two, three) {
+    return one + " " + two + " " + three;
+}
+
+alert(combinator("one", "combined", "string"));
