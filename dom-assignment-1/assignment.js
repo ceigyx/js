6aaec40a1fd88581b5a0e1f443e76f72a949837b
ceigyx <aklypse_ceigyx@live.ca>
2020-07-03 19:04:06 -0400

    completed dom assignment 1

---
+++ dom-assignment-1/assignment.js
@@ -0,0 +1,15 @@
+const task = document.querySelector("#task-1");
+const taskAgain = document.getElementById("task-1");
+
+const title = document.querySelector("title");
+const titleAgain = document.head.querySelector("title");
+
+const h1 = document.querySelector("h1");
+
+task.style.backgroundColor = "black";
+taskAgain.style.color = "white";
+
+title.innerText = "Assignment = Solved!";
+titleAgain.innerText = "Assignment = Solved!";
+
+h1.innerText = "Assignment = Solved!";
