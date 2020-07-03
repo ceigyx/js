4163379659fea3320cb1deed56a707e56c976676
ceigyx <aklypse_ceigyx@live.ca>
2020-07-03 06:05:39 -0400

    completed assignment, started functions-2 > calculator app and bind()

--- functions-assignment-1/assignment.js
+++ functions-assignment-1/assignment.js
@@ -1,5 +1,23 @@
-function sayHello(name) {
-  console.log('Hi ' + name);
+const sayHello = (pre = "Hi", name = "no name") => {
+  console.log(pre + ", "+ name);
+};
+
+const checkInput = (cb, ...strings) => {
+  let concat = "";
+  if (!strings) {
+    cb();
+    return;
+  } else {
+    for (const string of strings) {
+      if (typeof(string) !== "string") {
+        cb();
+        return;
+      }
+      concat = concat + string;
+
+    }
+  }
+  return concat;
 }
 
 sayHello();
