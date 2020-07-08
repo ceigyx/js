f233b1ac286781391c261bba38d51faebf7bc113
ceigyx <aklypse_ceigyx@live.ca>
2020-07-08 12:04:01 -0400

    completed arrays module lessons, started arrays module assignment

---
+++ arrays/app.js
@@ -0,0 +1,41 @@
+// // const ids = new Set([1, 2, 3]);
+
+// // for (const entry of ids.entries()) {
+// //   console.log(entry[1]);
+// // }
+
+// // const person1 = { name: "max" };
+// // const person2 = { name: "manuel" };
+// // const [person3, person4] = [{ name: "brandon" }, { name: "linda" }];
+// // const personData = new Map([[person1, [{ date: "yesterday", price: 10 }]]]);
+// // personData.set(person2, [{date: "two weeks ago", price: 100}])
+
+// // for (const entry of personData.entries()) {
+// //   console.log(entry);
+// // }
+
+// // for (const [key, value] of personData.entries(0)) {
+// //   console.log(key, value)
+// // }
+
+// // for (const key of personData.keys()) {
+// //   console.log(key);
+// // }
+
+// // for (const value of personData.values(0)) {
+// //   console.log(value);
+// // }
+
+// // console.log(personData);
+
+// let person = {name: "linda"};
+
+// const personData = new WeakMap();
+
+// personData.set(person, "val");
+
+// console.log(personData);
+
+// person = null;
+
+// console.log(personData);
