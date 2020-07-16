fe9db5c560981ed7ec79263603cddafb92eab86d
ceigyx <aklypse_ceigyx@live.ca>
2020-07-16 16:57:19 -0400

    completed async module, started http module

--- async/app.js
+++ async/app.js
@@ -1,8 +1,64 @@
-const button = document.querySelector('button');
-const output = document.querySelector('p');
+const button = document.querySelector("button");
+const output = document.querySelector("p");
 
-function trackUserHandler() {
-  console.log('Clicked!');
+const getPosition = (opts) => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        resolve(success);
+      },
+      (error) => {
+        reject(error);
+      },
+      opts
+    );
+  });
+  return promise;
+};
+
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, duration);
+  });
+  return promise;
+};
+
+async function trackUserHandler() {
+  // let positionData;
+  let posData;
+  let timerData;
+  try {
+    posData = await getPosition();
+    timerData = await setTimer(2000);
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(timerData, posData);
+  // .then((posData) => {
+  //   positionData = posData;
+  //   return setTimer(2000);
+  // // })
+  // .then((data) => {
+  //   console.log(data, positionData);
+  // })
+  // .catch((error) => {
+  //   console.log(error);
+  // });
+  console.log("code after geolocation fetch");
 }
 
-button.addEventListener('click', trackUserHandler);
+Promise.race([getPosition(), setTimer(1000)]).then(data => {
+  console.log(data); //single fastest result, rest is ignored but still runs
+});
+
+Promise.all([getPosition(), setTimer(2000)]).then(promiseData => {
+  console.log(promiseData); //array of results
+})
+
+Promise.allSettled([getPosition(), setTimer(2000)]).then(data => {
+  console.log(data);
+})
+
+button.addEventListener("click", trackUserHandler);
