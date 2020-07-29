a5d2e23afd1f1dd341fb359dce877d94be56a2df
ceigyx <aklypse_ceigyx@live.ca>
2020-07-29 04:10:26 -0400

    midway through components module, started components assignment

--- components/src/components.js
+++ components/src/components.js
@@ -1,2 +1,5 @@
 import { Modal } from './components/modal';
 import { Tooltip } from './components/tooltip';
+import { ConfirmLink } from './components/confirm-link';
+
+console.log(Modal, Tooltip, ConfirmLink);
--- components/src/components/tooltip.js
+++ components/src/components/tooltip.js
@@ -1,10 +1,44 @@
 export class Tooltip extends HTMLElement {
-    constructor() {
-        super();
-        document.createElement('span');
-        
-        this.appendChild();
+  constructor() {
+    super();
+    this._tooltipContainer;
+    this._tooltipText = 'Default text';
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.innerHTML = `
+    <style>
+      div {
+        background-color: lightblue;
+        color: darkorange;
+        position: absolute;
+        z-index: 10;
+      }
+    </style>
+    <slot>Default</slot>
+    <span> (?) </span>
+    `;
+  }
+
+  connectedCallback() {
+    if (this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text');
     }
+    const tooltipIcon = this.shadowRoot.querySelector('span');
+    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+    this.shadowRoot.appendChild(tooltipIcon);
+    this.style.position = 'relative';
+  }
+
+  _showTooltip() {
+    this._tooltipContainer = document.createElement('div');
+    this._tooltipContainer.textContent = `${this._tooltipText}`;
+    this.shadowRoot.appendChild(this._tooltipContainer);
+  }
+
+  _hideTooltip() {
+    this.shadowRoot.removeChild(this._tooltipContainer);
+  }
 }
 
 customElements.define('uc-tooltip', Tooltip);
+
