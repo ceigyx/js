f5318b7d5409cc1fcc81b288979c138c98ec6d96
ceigyx <aklypse_ceigyx@live.ca>
2020-07-30 18:15:01 -0400

    completed styling components

--- components/src/components.js
+++ components/src/components.js
@@ -2,4 +2,6 @@ import { Modal } from './components/modal';
 import { Tooltip } from './components/tooltip';
 import { ConfirmLink } from './components/confirm-link';
 
-console.log(Modal, Tooltip, ConfirmLink);
+console.dir(Modal);
+console.dir(Tooltip);
+console.dir(ConfirmLink);
